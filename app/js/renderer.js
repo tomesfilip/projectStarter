be830ed9f9cf115b62609a499d66982baf3d37a2
Filip Tomes <filiptomes@protonmail.com>
2020-07-29 21:56:33 +0200

    creating github repository on submit

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,11 +1,10 @@
-const os = require('os')
+
+require('dotenv/config')
 
 const { ipcRenderer } = require('electron')
 const { dialog } = require('electron').remote
 const { Octokit } = require('@octokit/rest')
-const octokit = new Octokit({
-    auth: ""
-})
+
 
 let outputPath = document.querySelector('#path')
 const outputPathBtn = document.querySelector('#path-btn')
@@ -14,8 +13,9 @@ const gitRepo = document.querySelector('#repo')
 const nameInput = document.querySelector('#name')
 const projectInput = document.querySelector('#project')
 
-
-require("dotenv").config()
+const octokit = new Octokit({
+    auth: process.env.TOKEN,
+})
 
 // set output path for project
 outputPathBtn.addEventListener('click', (e) => {
@@ -30,19 +30,21 @@ outputPathBtn.addEventListener('click', (e) => {
     }        
 })
 
-const userWithAuth = new octokit({
-    auth: process.env.TOKEN
-})
+
 
 // on submit
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const projectName = nameInput.value
+    const projectName = nameInput.value.toString()
     const isRepo = gitRepo.checked
     const projectType = projectInput.value
     console.log(outputPath, projectName, isRepo, projectType)
-    userWithAuth.repos.createForAuthenticatedUser({
-        name: "testing"
-    })
+
+    if(isRepo){
+        octokit.repos.createForAuthenticatedUser({
+            name: projectName,
+            auto_init: true,
+        })
+    }    
 })
